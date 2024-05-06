26673bc9ff05991f771631e081dec32be98dc075
Ali Motameni <motameni1371@gmail.com>
2024-05-06 15:46:54 +0330

    Add 01.py to quera problems

---
+++ quera/01.py
@@ -0,0 +1,12 @@
+# https://quera.org/problemset/280
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+if (a ** 2 + b ** 2 == c ** 2) or \
+   (b ** 2 + c ** 2 == a ** 2) or \
+   (c ** 2 + a ** 2 == b ** 2):
+    print("YES")
+else:
+    print("NO")
